4bcf73ff14cfa58b6d5d4f73b7d7eab0a5b7379b
sun JinShuang <sunjinshuang@fangmaster.cn>
2019-03-20 20:00:03 +0800

    增加信息

--- src/main/java/com/nutFruit/food/web/controller/WelcomeController.java
+++ src/main/java/com/nutFruit/food/web/controller/WelcomeController.java
@@ -3,6 +3,12 @@ package com.nutFruit.food.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class WelcomeController {
@@ -18,6 +24,23 @@ public class WelcomeController {
     public String index(){
 
         return "/main.jsp";
+    }
+
+    /**
+     * @Author: sun JinShuang
+     * @return
+     */
+    @RequestMapping(path = "/rentCommunityInit",method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> getRenttingCommunity(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", 0);
+        map.put("msg", "");
+        //结果总数
+        map.put("count", "");
+        //结果对象数据
+        map.put("data", "");
+        return map;
     }
 
 }
